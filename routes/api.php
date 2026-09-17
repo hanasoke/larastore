0b7d619f18248f4bb03d7af27a1e9ce58457e5ff
Hanas <hanasoke@gmail.com>
2026-09-17 19:18:33 +0700

    added api test

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\KontakController;
 
 Route::post('/kontak', [KontakController::class, 'store']);
+
+Route::get('/test', function () {
+    return response()->json([
+        'success' => true,
+        'message' => 'Berhasil terhubung ke Laravel'
+    ]);
+});
