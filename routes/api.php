a008c835cf8543d2e8a794cfa951efe41d724c87
Hanas <hanasoke@gmail.com>
2026-09-18 22:54:01 +0700

    update added guru api routes

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\GuruController;
 use App\Http\Controllers\Api\KontakController;
 
-Route::post('/kontak', [KontakController::class, 'store']);
+Route::get('/guru', [GuruController::class, 'index']);
+
+Route::get('/guru/{slug}', [GuruController::class, 'show']);
+
+Route::post('/kontak', [
+    KontakController::class, 
+    'store'
+]);
 
 Route::get('/test', function () {
     return response()->json([
