917772a40019bd43b08e3444258e86ed3134d467
hanasbp <hanasoke@gmail.com>
2026-09-25 08:06:30 +0700

    update navbar color

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,5 +1,5 @@
  <!-- Navbar -->
-<nav class="navbar navbar-expand-lg bg-warning navbar-white">
+<nav class="navbar navbar-expand-lg bg-primary navbar-white">
     <div class="container">
 
         <a class="navbar-brand fw-bold" href="#">
